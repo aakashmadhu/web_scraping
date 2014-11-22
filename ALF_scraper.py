696b9fc8e7d327999abc1d49061f4fce61afff6d
Aakash <aakashmadhu@live.com>
2014-11-21 22:50:28 -0500

    Update ALF_scraper.py

--- ALF_scraper.py
+++ ALF_scraper.py
@@ -66,7 +66,7 @@ index_url = root_url + States[0] + '.htm'
 def get_facility_names():
     response = requests.get(index_url)
     soup = bs4.BeautifulSoup(response.text)
-    mydivs = soup.findAll("div", attrs = { "class" : "detailText" }).string	
+    mydivs = soup.findAll("div", attrs = { "class" : "detailText" }).string 
     print mydivs
 
 #get_facility_names()
@@ -76,13 +76,13 @@ def testing(webpage):
     soup = bs4.BeautifulSoup(response.text)    
     fo = open("ALF_list.txt", "wb")
     for i in soup.findAll('div', attrs = {'class' : 'detailText'}):
-        for j in i.findAll('p'): 	        	
-			facility_info =  j.get_text
-			total_facility_info = ""
-			total_facility_info += str(facility_info)
-			fo.write(total_facility_info)
-	print total_facility_info
-	    
+        for j in i.findAll('p'):            
+            facility_info =  j.get_text
+            total_facility_info = ""
+            total_facility_info += str(facility_info)
+            fo.write(total_facility_info)
+    print total_facility_info
+      
     fo.close()
     
     return total_facility_info
@@ -96,7 +96,7 @@ for State in States:
 #review_tag  = {'class':re.compile("detailText")}
 #   all_reviews = soup.findAll(attrs=review_tag)
 #for pfound in soup.findAll('p'):
-#    	print pfound
+#     print pfound
 #mydivs = soup.find("div", attrs = { "class" : "detailText" }).findAll('p')
 """    
 #facilities = soup.content('div', class_='detailText')
